241382bfe2591a14d8f50815075aac4ed4e6e159
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-03-27 11:10:00 +0300

    auth was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import jwtDecode from 'jwt-decode';
 import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin } from '@react-oauth/google';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div id='signInButton'>nl</div>
+      </div>
+      <GoogleLogin
+        onSuccess={credentialResponse => {
+          console.log(credentialResponse);
+          const userObject = jwtDecode(credentialResponse.credential || "");
+          console.log(userObject)
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const CLIENT_ID ='636556553285-hni12rho60hnij6acjrhhv6t2al7i21p.apps.googleusercontent.com';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
