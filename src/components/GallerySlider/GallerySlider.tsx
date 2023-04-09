aadf328976eb60a9ace4d8b980f61bfa566d31fc
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-04-09 12:01:14 +0300

    gallery was made

---
+++ src/components/GallerySlider/GallerySlider.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import './GallerySlider.scss'
+import { Link } from 'react-router-dom';
+
+interface Props {
+  images: string[];
+  currentImage: number;
+  setCurrentImage: React.Dispatch<React.SetStateAction<number>>
+}
+
+export const GallerySlider: React.FC<Props> = ({ images, currentImage, setCurrentImage }) => {
+  const handleClick = (index: number) => {
+    setCurrentImage(index + 1);
+  };
+
+  return (
+    <div
+      className='gallery-slider__container'
+    >
+
+      <button
+        onClick={() => setCurrentImage(0)}
+        className='gallery-slider__btn--close'
+      >
+      </button>
+
+      <div className='gallery-slider__section'>
+        <img
+          className='gallery-slider__main'
+          src={images[currentImage - 1]}
+          alt="Gallery"
+        />
+
+        <button
+          className='gallery-slider__btn--prev'
+          onClick={() => setCurrentImage(prev => prev - 1 === 0 ? 1 : prev - 1)}>
+        </button>
+
+        <button
+          className='gallery-slider__btn--next'
+          onClick={() => setCurrentImage(prev => prev + 1 === images.length + 1 ? images.length : prev + 1)}>
+        </button>
+      </div>
+
+      <span className='gallery-slider__amount text-xx-white-400'>
+        {`${currentImage} / ${images.length}`}
+      </span>
+
+      <div className='gallery-slider__photos'>
+        {images.map((image, index) => (
+          <img
+            className='gallery-slider__photo'
+            key={index}
+            src={image}
+            alt={`Gallery ${index}`}
+            onClick={() => handleClick(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+
+
